5b73a2ec4bfebfa38030b451a15362882d7ae23d
Alexandr Morozov <moralex260@gmail.com>
2017-11-10 18:35:16 +0000

    initial

--- js/fib2.js
+++ js/fib2.js
@@ -3,17 +3,17 @@ function prn(val) {
 prn('розрахунок числа фібоначі по порядковому номеру<br>');
 const n = Number(prompt('введіть порядковий номер числа фібоначі,величину якого ви ,хочете знайти:'));
 prn('порядковий номер числа:<br>' + n);
-//function fib(i) {
+//function fib(n) {
  //   if (n <= 0) return 0;
   //  else if (n == 1) return 1;
   //  else return fib(n - 1) + fib(n - 2);
 //}
-//let res = fib(i);
+//let res = fib(n);
 //prn('відповідь:' + res);
 
 let fib =[1,1];
 for (let i = 2; i < n; i++) {
-    fib[i] = fib[n - 1] + fib[n - 2];
+    fib[i] = fib[i - 1] + fib[i- 2];
 
 let a=fib[i];
 prn('відповідь:' +a );}
